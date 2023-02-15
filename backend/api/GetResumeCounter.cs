58458d847f1ed99fc718e59e5d35b288095bd7e8
Sanel Pihic <sanel.pihic@gmail.com>
2023-02-15 13:57:19 +0000

    updated getresumecounter-file with updated parameters for newer version of sdk.

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Text;
-
 namespace Company.Function
 {
     public static class NewBaseType
@@ -17,17 +16,14 @@ namespace Company.Function
         [FunctionName("GetResumeCounter")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName: "AzResumeDB", collectionName: "CounterContainer", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName: "AzResumeDB", collectionName: "CounterContainer", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            [CosmosDB(databaseName: "AzResumeDB", containerName: "CounterContainer", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName: "AzResumeDB", containerName: "CounterContainer", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
             updatedCounter = counter;
             updatedCounter.Count += 1;
-
             var jsonToReturn = JsonConvert.SerializeObject(counter);
-
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
                 Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
